7ee1531975db55978242ebb98d1650964db8c2b3
shivam4198 <97816703+shivam4198@users.noreply.github.com>
2022-06-20 22:19:39 -0400

    updated the views.py

--- posts/views.py
+++ posts/views.py
@@ -24,7 +24,7 @@ def index(request):
             return HttpResponseRedirect(form.errors.as_json())
     # Get sll posts, limit=20
 
-    posts = Post.objects.all()[:20]
+    posts = Post.objects.all().order_by('-created_at')[:20]
     # show
     return render(request, 'posts.html',
                   {'posts': posts})
